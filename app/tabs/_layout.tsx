d78ab56c260b5407669a4430133c51cb878ed74a
Karim Badwork <karim@badwork.fr>
2025-11-29 00:08:55 +0100

    feat: Ajout de l'écran "job" dans la navigation du layout

--- app/tabs/_layout.tsx
+++ app/tabs/_layout.tsx
@@ -16,6 +16,7 @@ export default function AppLayout() {
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="signin" options={{ headerShown: false }} />
       <Stack.Screen name="signup" options={{ headerShown: false }} />
+      <Stack.Screen name="job" options={{ headerShown: true }} />
     </Stack>
   );
 }
