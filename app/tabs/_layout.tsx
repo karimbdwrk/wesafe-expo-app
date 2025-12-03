de98c02b140781e2f18182f1d9bf495044a396ae
Karim Badwork <karim@badwork.fr>
2025-12-03 01:56:20 +0100

    refactor: Commenter les routes 'signin' et 'signup' pour un code plus propre dans AppLayout

--- app/tabs/_layout.tsx
+++ app/tabs/_layout.tsx
@@ -14,8 +14,8 @@ export default function AppLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="signin" options={{ headerShown: false }} />
-      <Stack.Screen name="signup" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="signin" options={{ headerShown: false }} />
+      <Stack.Screen name="signup" options={{ headerShown: false }} /> */}
     </Stack>
   );
 }
