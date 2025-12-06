294038f88b416e3dbc26f6f11dc3cef6cbc80f99
Karim Badwork <karim@badwork.fr>
2025-12-07 00:10:18 +0100

    fix: Ajouter le champ SIRET et réorganiser les champs de saisie dans l'écran de mise à jour de l'entreprise

--- app/updatecompany.jsx
+++ app/updatecompany.jsx
@@ -13,6 +13,7 @@ import { Check } from "lucide-react-native";
 
 import { useDataContext } from "@/context/DataContext";
 
+import { VStack } from "@/components/ui/vstack";
 import { Heading } from "@/components/ui/heading";
 import { Input, InputField } from "@/components/ui/input";
 import { Button, ButtonText } from "@/components/ui/button";
@@ -25,11 +26,13 @@ const UpdateCompany = () => {
 	const { companyName } = useLocalSearchParams();
 
 	const [name, setName] = useState("");
+	const [siret, setSiret] = useState("");
 
 	const handleUpdateCompany = async () => {
 		try {
 			const updateCompany = await update("companies", user.id, {
 				name: name,
+				siret: siret,
 			});
 			console.log("update company :", updateCompany);
 			toast.success("Operation successful!", {
@@ -51,14 +54,24 @@ const UpdateCompany = () => {
 		<SafeAreaView>
 			<View style={{ padding: 15 }}>
 				<Heading>Update Company</Heading>
-				<Input className='min-w-[250px]'>
-					<InputField
-						type='text'
-						placeholder='Entrez le nom de votre entreprise'
-						value={name}
-						onChangeText={setName}
-					/>
-				</Input>
+				<VStack style={{ gap: 15, marginTop: 20, marginBottom: 20 }}>
+					<Input className='min-w-[250px]'>
+						<InputField
+							type='text'
+							placeholder='Entrez le nom de votre entreprise'
+							value={name}
+							onChangeText={setName}
+						/>
+					</Input>
+					<Input className='min-w-[250px]'>
+						<InputField
+							type='number'
+							placeholder='Entrez le SIRET de votre entreprise'
+							value={siret}
+							onChangeText={setSiret}
+						/>
+					</Input>
+				</VStack>
 				<Button className='ml-auto' onPress={handleUpdateCompany}>
 					<ButtonText className='text-typography-0'>
 						Suivant
