3ce4c5f91d0bd3aeab3d3ed02785bec5cf2d52a5
Karim Badwork <karim@badwork.fr>
2026-01-26 23:35:59 +0100

    feat: Ajouter le composant Documents avec navigation vers la vérification d'identité

---
+++ app/documents.jsx
@@ -0,0 +1,24 @@
+import { useRouter, useFocusEffect } from "expo-router";
+
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
+import { HStack } from "@/components/ui/hstack";
+import { Button, ButtonText } from "@/components/ui/button";
+
+const Documents = () => {
+	const router = useRouter();
+
+	return (
+		<Box flex={1} bg='$backgroundLight0' style={{ padding: 15 }}>
+			<VStack space='xl' p='$6'>
+				<Text>Documents</Text>
+				<Button onPress={() => router.push("/iddocumentverification")}>
+					<ButtonText>ID Document</ButtonText>
+				</Button>
+			</VStack>
+		</Box>
+	);
+};
+
+export default Documents;
