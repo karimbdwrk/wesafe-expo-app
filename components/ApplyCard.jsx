2f9d00776feed6341832a7fb6f73f3447e3a7f8e
Karim Badwork <karim@badwork.fr>
2026-01-31 00:25:49 +0100

    feat: Remplacer le composant Card par un Pressable pour améliorer l'interaction utilisateur dans ApplyCard

--- components/ApplyCard.jsx
+++ components/ApplyCard.jsx
@@ -5,12 +5,15 @@ import { useRouter } from "expo-router";
 import { Card } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { HStack } from "@/components/ui/hstack";
+import { Pressable } from "@/components/ui/pressable";
 // import { Image } from "@/components/ui/image";
 // import { Link, LinkText } from "@/components/ui/link";
 import { Text } from "@/components/ui/text";
 import { Button, ButtonText } from "@/components/ui/button";
 import { Badge, BadgeIcon, BadgeText } from "@/components/ui/badge";
 import { GlobeIcon } from "@/components/ui/icon";
+import { width } from "dom-helpers";
+import { BackpackIcon } from "lucide-react-native";
 
 const ApplyCard = ({
 	id,
@@ -20,53 +23,53 @@ const ApplyCard = ({
 	company_id,
 	isRefused,
 	apply_id,
+	status,
 }) => {
 	const router = useRouter();
 	return (
-		<Card size='lg' variant='filled' style={styles.card}>
-			{name && (
-				<Heading size='md' className='mb-1'>
-					{name}
-				</Heading>
+		<Pressable
+			onPress={() =>
+				router.push({
+					pathname: "/application",
+					params: {
+						id,
+						title,
+						company_id,
+						category,
+						apply_id,
+						name,
+					},
+				})
+			}>
+			{({ pressed }) => (
+				<Card
+					size='lg'
+					variant='filled'
+					style={{
+						width: "100%",
+						backgroundColor: pressed ? "#F0F0F0" : "#F7F7F7",
+					}}>
+					{name && (
+						<Heading size='md' className='mb-1'>
+							{name}
+						</Heading>
+					)}
+					<Heading size='md' className='mb-1'>
+						{title}
+					</Heading>
+					<Text>{id}</Text>
+					<HStack style={{ marginTop: 15, gap: 10 }}>
+						<Badge size='md' variant='solid' action='warning'>
+							<BadgeText>{category}</BadgeText>
+						</Badge>
+						<Badge size='md' variant='solid' action='info'>
+							<BadgeText>{status}</BadgeText>
+						</Badge>
+					</HStack>
+				</Card>
 			)}
-			<Heading size='md' className='mb-1'>
-				{title}
-			</Heading>
-			<Text>{id}</Text>
-			<HStack style={{ paddingVertical: 15, gap: 10 }}>
-				<Badge size='md' variant='solid' action='warning'>
-					<BadgeText>{category}</BadgeText>
-				</Badge>
-				{isRefused && (
-					<Badge size='md' variant='solid' action='error'>
-						<BadgeText>Refusé</BadgeText>
-					</Badge>
-				)}
-			</HStack>
-			<Button
-				onPress={() =>
-					router.push({
-						pathname: "/application",
-						params: {
-							id,
-							title,
-							company_id,
-							category,
-							apply_id,
-							name,
-						},
-					})
-				}>
-				<ButtonText>Voir la candidature</ButtonText>
-			</Button>
-		</Card>
+		</Pressable>
 	);
 };
-
-const styles = StyleSheet.create({
-	card: {
-		width: "100%",
-	},
-});
 
 export default ApplyCard;
