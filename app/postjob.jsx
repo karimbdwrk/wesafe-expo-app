9880607aec9d0f01795c8a72406482a8887b0781
Karim Badwork <karim@badwork.fr>
2026-02-18 03:42:56 +0100

    feat: ajouter le composant PostJob pour la publication d'offres

---
+++ app/postjob.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
+
+const PostJob = () => {
+	return (
+		<Box>
+			<Text>PostJob screen</Text>
+		</Box>
+	);
+};
+
+export default PostJob;
