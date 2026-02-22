d146f40c2d5afb0f7ff3546629dd629ddcb1bca6
Karim Badwork <karim@badwork.fr>
2026-02-22 22:40:16 +0100

    feat: ajouter la gestion des salaires et améliorer l'affichage des catégories dans le composant ApplyCard

--- components/ApplyCard.jsx
+++ components/ApplyCard.jsx
@@ -17,6 +17,7 @@ import {
 	AvatarImage,
 	AvatarFallbackText,
 } from "@/components/ui/avatar";
+import { Divider } from "@/components/ui/divider";
 
 import {
 	Briefcase,
@@ -28,9 +29,14 @@ import {
 	IdCard,
 	ChevronRight,
 	MapPin,
+	BadgeEuro,
 } from "lucide-react-native";
 
 import { useTheme } from "@/context/ThemeContext";
+import { height, width } from "dom-helpers";
+
+// Tableau des catégories
+import { getCategoryLabel } from "@/constants/categories";
 
 const STATUS_ORDER = [
 	"applied",
@@ -85,6 +91,19 @@ const ApplyCard = ({
 	apply_id,
 	application,
 	status,
+	contract_type,
+	working_time,
+	salary_hourly,
+	salary_amount,
+	salary_min,
+	salary_max,
+	salary_type,
+	salary_monthly_fixed,
+	salary_monthly_min,
+	salary_monthly_max,
+	salary_annual_fixed,
+	salary_annual_min,
+	salary_annual_max,
 }) => {
 	const router = useRouter();
 	const { user, accessToken, role } = useAuth();
@@ -99,6 +118,35 @@ const ApplyCard = ({
 	const [companyNotification, setCompanyNotification] = useState(
 		application?.company_notification || false,
 	);
+
+	const formatSalary = (salary_type) => {
+		if (!salary_type) return "Non spécifié";
+
+		switch (salary_type) {
+			case "selon_profil":
+				return "Selon profil";
+			case "hourly":
+				return salary_hourly ? `${salary_hourly}€/h` : "Non spécifié";
+			case "monthly_fixed":
+				return salary_monthly_fixed
+					? `${salary_monthly_fixed}€/mois`
+					: "Non spécifié";
+			case "annual_fixed":
+				return salary_annual_fixed
+					? `${salary_annual_fixed}€/an`
+					: "Non spécifié";
+			case "monthly_range":
+				return salary_monthly_min && salary_monthly_max
+					? `${salary_monthly_min}€ - ${salary_monthly_max}€/mois`
+					: "Non spécifié";
+			case "annual_range":
+				return salary_annual_min && salary_annual_max
+					? `${salary_annual_min}€ - ${salary_annual_max}€/an`
+					: "Non spécifié";
+			default:
+				return "Non spécifié";
+		}
+	};
 
 	// Synchroniser avec les props si elles changent
 	useEffect(() => {
@@ -201,10 +249,25 @@ const ApplyCard = ({
 							? "#4b5563"
 							: "#e5e7eb",
 				}}>
+				<VStack
+					style={{
+						position: "absolute",
+						top: 16,
+						right: 16,
+						zIndex: 10,
+					}}>
+					{/* Unread Messages */}
+					{unreadMessagesCount > 0 && (
+						<Badge size='sm' variant='solid' action='error'>
+							<BadgeIcon as={MessageCircle} className='mr-2' />
+							<BadgeText>{unreadMessagesCount}</BadgeText>
+						</Badge>
+					)}
+				</VStack>
 				<HStack
 					space='md'
 					className='items-center justify-between mb-2'>
-					<VStack space='md'>
+					<VStack space='md' style={{ width: "90%" }}>
 						{/* Header with title and company */}
 						<VStack space='xs'>
 							<Heading
@@ -215,7 +278,15 @@ const ApplyCard = ({
 								}}>
 								{title}
 							</Heading>
-							{name && (
+							{role === "pro" && (
+								<HStack space='xs' className='items-center'>
+									<MapPin size={12} />
+									<Text size='sm'>
+										{application?.jobs?.city}
+									</Text>
+								</HStack>
+							)}
+							{/* {name && (
 								<Text
 									size='sm'
 									style={{
@@ -223,17 +294,64 @@ const ApplyCard = ({
 									}}>
 									{name}
 								</Text>
-							)}
+							)} */}
 						</VStack>
+
+						{/* {role === "candidat" ? (
+							<HStack space='md' className='items-center'>
+								<Avatar size='md'>
+									<AvatarFallbackText>
+										{application?.companies?.name}
+									</AvatarFallbackText>
+									<AvatarImage
+										source={{
+											uri: application?.companies
+												?.logo_url,
+										}}
+									/>
+								</Avatar>
+								<VStack>
+									<Heading
+										size='md'
+										style={{
+											color: isDark
+												? "#f3f4f6"
+												: "#111827",
+										}}>
+										{application?.companies?.name}
+									</Heading>
+									<HStack space='xs' className='items-center'>
+										<MapPin size={12} />
+										<Text size='sm'>
+											{application?.jobs?.city}
+										</Text>
+									</HStack>
+								</VStack>
+							</HStack>
+						) : (
+							<HStack space='xs' className='items-center'>
+								<MapPin size={12} />
+								<Text size='sm'>{application?.jobs?.city}</Text>
+							</HStack>
+						)} */}
 
 						<HStack space='md' className='items-center'>
 							<Avatar size='md'>
 								<AvatarFallbackText>
-									{application?.companies?.name}
+									{role === "candidat"
+										? application?.companies?.name
+										: application?.profiles?.firstname +
+											" " +
+											application?.profiles?.lastname}
 								</AvatarFallbackText>
 								<AvatarImage
 									source={{
-										uri: application?.companies?.logo_url,
+										uri:
+											role === "candidat"
+												? application?.companies
+														?.logo_url
+												: application?.profiles
+														?.avatar_url,
 									}}
 								/>
 							</Avatar>
@@ -243,28 +361,57 @@ const ApplyCard = ({
 									style={{
 										color: isDark ? "#f3f4f6" : "#111827",
 									}}>
-									{application?.companies?.name}
+									{role === "candidat"
+										? application?.companies?.name
+										: application?.profiles?.firstname +
+											" " +
+											application?.profiles?.lastname}
 								</Heading>
-								<HStack space='xs' className='items-center'>
+								{/* <HStack space='xs' className='items-center'>
 									<MapPin size={12} />
 									<Text size='sm'>
 										{application?.jobs?.city}
 									</Text>
-								</HStack>
+								</HStack> */}
 							</VStack>
 						</HStack>
 
-						{/* Info row with badges */}
-						<HStack
-							space='sm'
-							style={{ alignItems: "center", flexWrap: "wrap" }}
-							className='mt-2'>
+						<VStack>
 							{/* Category */}
-							<Badge size='sm' variant='solid' action='muted'>
-								<BadgeIcon as={IdCard} className='mr-2' />
-								<BadgeText>{category}</BadgeText>
-							</Badge>
-
+							<HStack>
+								<Badge size='sm' variant='solid' action='muted'>
+									<BadgeIcon as={IdCard} className='mr-2' />
+									<BadgeText>
+										{getCategoryLabel(category)}
+									</BadgeText>
+								</Badge>
+							</HStack>
+							{/* Info row with badges */}
+							<HStack
+								space='sm'
+								style={{
+									alignItems: "center",
+									flexWrap: "wrap",
+								}}
+								className='mt-2'>
+								{salary_type && (
+									<Badge
+										size='sm'
+										variant='solid'
+										action='warning'>
+										<BadgeIcon
+											as={BadgeEuro}
+											className='mr-2'
+										/>
+										<BadgeText>
+											{formatSalary(salary_type)}
+										</BadgeText>
+									</Badge>
+								)}
+							</HStack>
+						</VStack>
+						<Divider />
+						<HStack>
 							{/* Status */}
 							<Badge
 								size='sm'
@@ -276,26 +423,22 @@ const ApplyCard = ({
 								/>
 								<BadgeText>{statusConfig.title}</BadgeText>
 							</Badge>
-
-							{/* Unread Messages */}
-							{unreadMessagesCount > 0 && (
-								<Badge size='sm' variant='solid' action='error'>
-									<BadgeIcon
-										as={MessageCircle}
-										className='mr-2'
-									/>
-									<BadgeText>{unreadMessagesCount}</BadgeText>
-								</Badge>
-							)}
 						</HStack>
 					</VStack>
-					<Icon
-						as={ChevronRight}
-						size='lg'
+					<VStack
 						style={{
-							color: isDark ? "#9ca3af" : "#6b7280",
-						}}
-					/>
+							alignItems: "center",
+							width: "10%",
+							justifyContent: "center",
+						}}>
+						<Icon
+							as={ChevronRight}
+							size='lg'
+							style={{
+								color: isDark ? "#9ca3af" : "#6b7280",
+							}}
+						/>
+					</VStack>
 				</HStack>
 			</Card>
 		</TouchableOpacity>
