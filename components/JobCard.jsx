34ac78b7b07731976a932233bc2a7b276ad5ae15
Karim Badwork <karim@badwork.fr>
2026-02-23 02:17:41 +0100

    feat: refactor la fonction formatSalary en l'extrayant vers un fichier de constantes et en l'intégrant dans JobCard

--- components/JobCard.jsx
+++ components/JobCard.jsx
@@ -35,6 +35,7 @@ import {
 } from "lucide-react-native";
 import { width } from "dom-helpers";
 import { getCategoryLabel } from "@/constants/categories";
+import { formatSalary } from "@/constants/salary";
 
 const JobCard = ({
 	id,
@@ -97,35 +98,6 @@ const JobCard = ({
 				icon: <Check />,
 			},
 		);
-	};
-
-	const formatSalary = (salary_type) => {
-		if (!salary_type) return "Non spécifié";
-
-		switch (salary_type) {
-			case "selon_profil":
-				return "Selon profil";
-			case "hourly":
-				return salary_hourly ? `${salary_hourly}€/h` : "Non spécifié";
-			case "monthly_fixed":
-				return salary_monthly_fixed
-					? `${salary_monthly_fixed}€/mois`
-					: "Non spécifié";
-			case "annual_fixed":
-				return salary_annual_fixed
-					? `${salary_annual_fixed}€/an`
-					: "Non spécifié";
-			case "monthly_range":
-				return salary_monthly_min && salary_monthly_max
-					? `${salary_monthly_min}€ - ${salary_monthly_max}€/mois`
-					: "Non spécifié";
-			case "annual_range":
-				return salary_annual_min && salary_annual_max
-					? `${salary_annual_min}€ - ${salary_annual_max}€/an`
-					: "Non spécifié";
-			default:
-				return "Non spécifié";
-		}
 	};
 
 	return (
@@ -271,7 +243,16 @@ const JobCard = ({
 							<Badge size='sm' variant='solid' action='warning'>
 								<BadgeIcon as={BadgeEuro} className='mr-2' />
 								<BadgeText>
-									{formatSalary(salary_type)}
+									{formatSalary({
+										salary_type,
+										salary_hourly,
+										salary_monthly_fixed,
+										salary_annual_fixed,
+										salary_monthly_min,
+										salary_monthly_max,
+										salary_annual_min,
+										salary_annual_max,
+									})}
 								</BadgeText>
 							</Badge>
 						)}
