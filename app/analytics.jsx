87ce2d9377fe40806f8ffce4231f4dee39469c86
Karim Badwork <karim@badwork.fr>
2026-03-11 23:46:03 +0100

    feat: ajouter le composant Analytics avec un affichage de HomeChartsPro

---
+++ app/analytics.jsx
@@ -0,0 +1,12 @@
+import HomeChartsPro from "../components/HomeChartsPro";
+import { ScrollView } from "react-native-gesture-handler";
+
+const Analytics = () => {
+	return (
+		<ScrollView style={{ flex: 1, padding: 16 }}>
+			<HomeChartsPro />
+		</ScrollView>
+	);
+};
+
+export default Analytics;
