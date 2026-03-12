be8722589dd7e4c7de472a6d4e6e5398937d8ad0
Karim Badwork <karim@badwork.fr>
2026-03-12 09:54:52 +0100

    feat: améliorer l'animation du scanner avec un dégradé et ajustements de style

--- components/ScannerAnimation.jsx
+++ components/ScannerAnimation.jsx
@@ -1,70 +1,138 @@
 import React, { useEffect, useRef } from "react";
 import { View, Animated, StyleSheet } from "react-native";
-import Svg, { Rect, Path, G, Line } from "react-native-svg";
+import Svg, {
+	Rect,
+	G,
+	Line,
+	Defs,
+	LinearGradient,
+	Stop,
+} from "react-native-svg";
+
+const FRAME = 260;
+const LINE_TRAVEL = FRAME - 4;
+const CORNER = 30;
+const STROKE = 3;
+const CORNER_COLOR = "#ffffff";
+const LINE_COLOR = "#2563eb";
 
 const ScannerAnimation = () => {
 	const animation = useRef(new Animated.Value(0)).current;
 
 	useEffect(() => {
 		Animated.loop(
-			Animated.timing(animation, {
-				toValue: 1,
-				duration: 2500,
-				useNativeDriver: true,
-			})
+			Animated.sequence([
+				Animated.timing(animation, {
+					toValue: 1,
+					duration: 2000,
+					useNativeDriver: true,
+				}),
+				Animated.timing(animation, {
+					toValue: 0,
+					duration: 2000,
+					useNativeDriver: true,
+				}),
+			]),
 		).start();
-	}, [animation]);
+	}, []);
 
 	const translateY = animation.interpolate({
 		inputRange: [0, 1],
-		outputRange: [0, 198],
+		outputRange: [2, LINE_TRAVEL],
 	});
 
 	return (
-		<View style={styles.container}>
-			<Svg width='200' height='200' viewBox='0 0 200 200'>
-				{/* Frame corners */}
-				{/* <Path d='M5,5 h30 v10 h-20 v20 h-10 z' fill='black' />
-				<Path d='M195,5 h-30 v10 h20 v20 h10 z' fill='black' />
-				<Path d='M5,195 h30 v-10 h-20 v-20 h-10 z' fill='black' />
-				<Path d='M195,195 h-30 v-10 h20 v-20 h10 z' fill='black' /> */}
-
-				<G stroke='black' strokeWidth='3'>
+		<View style={{ width: FRAME, height: FRAME, position: "relative" }}>
+			<Svg width={FRAME} height={FRAME} viewBox={`0 0 ${FRAME} ${FRAME}`}>
+				<G
+					stroke={CORNER_COLOR}
+					strokeWidth={STROKE}
+					strokeLinecap='round'>
 					{/* Top Left */}
-					<Line x1='10' y1='10' x2='40' y2='10' />
-					<Line x1='10' y1='10' x2='10' y2='40' />
+					<Line x1='10' y1='10' x2={10 + CORNER} y2='10' />
+					<Line x1='10' y1='10' x2='10' y2={10 + CORNER} />
 					{/* Top Right */}
-					<Line x1='160' y1='10' x2='190' y2='10' />
-					<Line x1='190' y1='10' x2='190' y2='40' />
+					<Line
+						x1={FRAME - 10 - CORNER}
+						y1='10'
+						x2={FRAME - 10}
+						y2='10'
+					/>
+					<Line
+						x1={FRAME - 10}
+						y1='10'
+						x2={FRAME - 10}
+						y2={10 + CORNER}
+					/>
 					{/* Bottom Left */}
-					<Line x1='10' y1='160' x2='10' y2='190' />
-					<Line x1='10' y1='190' x2='40' y2='190' />
+					<Line
+						x1='10'
+						y1={FRAME - 10 - CORNER}
+						x2='10'
+						y2={FRAME - 10}
+					/>
+					<Line
+						x1='10'
+						y1={FRAME - 10}
+						x2={10 + CORNER}
+						y2={FRAME - 10}
+					/>
 					{/* Bottom Right */}
-					<Line x1='190' y1='160' x2='190' y2='190' />
-					<Line x1='160' y1='190' x2='190' y2='190' />
+					<Line
+						x1={FRAME - 10}
+						y1={FRAME - 10 - CORNER}
+						x2={FRAME - 10}
+						y2={FRAME - 10}
+					/>
+					<Line
+						x1={FRAME - 10 - CORNER}
+						y1={FRAME - 10}
+						x2={FRAME - 10}
+						y2={FRAME - 10}
+					/>
 				</G>
-
-				{/* Stylized QR code blocks */}
-				{/* <G>
-					<Rect x='60' y='60' width='10' height='10' fill='#000' />
-					<Rect x='85' y='85' width='8' height='8' fill='#000' />
-					<Rect x='110' y='60' width='8' height='8' fill='#000' />
-					<Rect x='135' y='85' width='10' height='10' fill='#000' />
-					<Rect x='145' y='115' width='6' height='6' fill='#000' />
-					<Rect x='90' y='130' width='10' height='10' fill='#000' />
-				</G> */}
 			</Svg>
 
-			{/* Scanning line */}
+			{/* Scan line animée */}
 			<Animated.View
-				style={[
-					styles.scanLineContainer,
-					{
-						transform: [{ translateY }],
-					},
-				]}>
-				<Svg width='200' height='2'>
-					<Rect x='5' y='0' width='190' height='2' fill='red' />
+				style={[styles.scanLine, { transform: [{ translateY }] }]}>
+				<Svg width={FRAME} height={4}>
+					<Defs>
+						<LinearGradient
+							id='lineGrad'
+							x1='0'
+							y1='0'
+							x2='1'
+							y2='0'>
+							<Stop
+								offset='0'
+								stopColor={LINE_COLOR}
+								stopOpacity='0'
+							/>
+							<Stop
+								offset='0.2'
+								stopColor={LINE_COLOR}
+								stopOpacity='1'
+							/>
+							<Stop
+								offset='0.8'
+								stopColor={LINE_COLOR}
+								stopOpacity='1'
+							/>
+							<Stop
+								offset='1'
+								stopColor={LINE_COLOR}
+								stopOpacity='0'
+							/>
+						</LinearGradient>
+					</Defs>
+					<Rect
+						x='0'
+						y='1'
+						width={FRAME}
+						height='2'
+						fill='url(#lineGrad)'
+					/>
 				</Svg>
 			</Animated.View>
 		</View>
@@ -72,12 +140,7 @@ const ScannerAnimation = () => {
 };
 
 const styles = StyleSheet.create({
-	container: {
-		width: 200,
-		height: 200,
-		position: "relative",
-	},
-	scanLineContainer: {
+	scanLine: {
 		position: "absolute",
 		top: 0,
 		left: 0,
