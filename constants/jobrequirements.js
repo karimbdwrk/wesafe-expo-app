5ee8be6c74b8835d010d09b2268cdf6623c08e09
Karim Badwork <karim@badwork.fr>
2026-03-20 03:07:45 +0100

    feat(jobrequirements): normaliser les noms des champs et améliorer la cohérence des exigences

--- constants/jobrequirements.js
+++ constants/jobrequirements.js
@@ -1,160 +1,97 @@
 export const JOB_REQUIREMENTS = {
 	aps: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: ["tfp_aps", "cap_apsh", "bac_pro_securite"],
-		certifications: ["sst"],
-		driving_licenses: [],
-	},
-
-	ads: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: ["tfp_aps", "cap_apsh", "bac_pro_securite"],
-		certifications: ["sst"],
+		cnaps: ["SURVEILLANCE_HUMAINE"],
+		diplomas: ["TFP_APS", "CAP_APS", "BAC_PRO_SECURITE"],
+		certifications: ["SST"],
 		driving_licenses: [],
 	},
 
 	ads_magasin: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: ["tfp_aps"],
-		certifications: ["sst"],
-		driving_licenses: [],
-	},
-
-	ads_filtrage: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: ["tfp_aps"],
-		certifications: ["palpation", "sst"],
+		cnaps: ["SURVEILLANCE_HUMAINE"],
+		diplomas: ["TFP_APS"],
+		certifications: ["SST"],
 		driving_licenses: [],
 	},
 
 	ads_ronde: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: ["tfp_aps"],
-		certifications: ["sst"],
+		cnaps: ["SURVEILLANCE_HUMAINE"],
+		diplomas: ["TFP_APS"],
+		certifications: ["SST"],
+		driving_licenses: ["B"],
+	},
+
+	agent_mobile: {
+		cnaps: ["SURVEILLANCE_HUMAINE"],
+		diplomas: ["TFP_APS"],
+		certifications: ["SST"],
 		driving_licenses: ["B"],
 	},
 
 	agent_evenementiel: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: ["tfp_aps"],
-		certifications: ["palpation", "sst"],
+		cnaps: ["SURVEILLANCE_HUMAINE"],
+		diplomas: ["TFP_APS"],
+		certifications: ["PALPATION", "SST"],
 		driving_licenses: [],
 	},
 
-	agent_mobile: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: ["tfp_aps"],
-		certifications: ["sst"],
-		driving_licenses: ["B"],
-	},
-
 	ssiap_1: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: ["ssiap_1"],
-		certifications: ["sst", "recyclage_ssiap"],
+		cnaps: ["SURVEILLANCE_HUMAINE"],
+		diplomas: ["SSIAP_1"],
+		certifications: ["SST"],
 		driving_licenses: [],
 	},
 
 	ssiap_2: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: ["ssiap_2"],
-		certifications: ["sst", "recyclage_ssiap"],
+		cnaps: ["SURVEILLANCE_HUMAINE"],
+		diplomas: ["SSIAP_2"],
+		certifications: ["SST"],
 		driving_licenses: [],
 	},
 
 	ssiap_3: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: ["ssiap_3"],
-		certifications: ["recyclage_ssiap"],
+		cnaps: ["SURVEILLANCE_HUMAINE"],
+		diplomas: ["SSIAP_3"],
+		certifications: [],
 		driving_licenses: [],
 	},
 
-	asc: {
-		cnaps: ["surveillance_humaine", "cynophile"],
-		diplomas: ["tfp_asc"],
-		certifications: ["sst", "habilitation_chien"],
+	cynophile: {
+		cnaps: ["SURVEILLANCE_HUMAINE", "CYNOPHILE"],
+		diplomas: ["TFP_ASC"],
+		certifications: ["SST"],
 		driving_licenses: ["B"],
 	},
 
 	apr: {
-		cnaps: ["protection_rapprochee"],
-		diplomas: ["tfp_apr"],
-		certifications: ["secourisme_tactique"],
+		cnaps: ["PROTECTION_RAPPROCHEE"],
+		diplomas: ["TFP_APR"],
+		certifications: [],
 		driving_licenses: ["B"],
 	},
 
-	apr_arme: {
-		cnaps: ["protection_rapprochee"],
-		diplomas: ["tfp_apr"],
-		certifications: ["formation_tir", "secourisme_tactique"],
-		driving_licenses: ["B"],
-	},
-
-	operateur_videosurveillance: {
-		cnaps: ["videoprotection"],
-		diplomas: ["tfp_video"],
+	operateur_videoprotection: {
+		cnaps: ["VIDEOPROTECTION"],
+		diplomas: ["TFP_OVS"],
 		certifications: [],
 		driving_licenses: [],
 	},
 
-	operateur_pc: {
-		cnaps: ["videoprotection"],
-		diplomas: ["tfp_video"],
+	telesurveillance: {
+		cnaps: ["VIDEOPROTECTION"],
+		diplomas: ["TFP_ATS"],
 		certifications: [],
 		driving_licenses: [],
 	},
 
-	tele_surveillance: {
-		cnaps: ["videoprotection"],
-		diplomas: ["tfp_ats"],
-		certifications: [],
+	surete_aeroportuaire: {
+		cnaps: ["SURVEILLANCE_HUMAINE"],
+		diplomas: ["TFP_APS"],
+		certifications: ["FORMATION_AEROPORTUAIRE"],
 		driving_licenses: [],
-	},
-
-	agent_surete_aeroportuaire: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: ["tfp_aps"],
-		certifications: ["formation_aeroportuaire"],
-		driving_licenses: [],
-	},
-
-	chef_equipe_securite: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: [],
-		certifications: ["experience"],
-		driving_licenses: ["B"],
-	},
-
-	chef_site: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: [],
-		certifications: ["experience"],
-		driving_licenses: ["B"],
-	},
-
-	responsable_securite: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: [],
-		certifications: ["experience"],
-		driving_licenses: ["B"],
-	},
-
-	directeur_securite: {
-		cnaps: ["surveillance_humaine"],
-		diplomas: [],
-		certifications: ["experience"],
-		driving_licenses: ["B"],
 	},
 
 	transport_fonds: {
-		cnaps: ["transport_fonds"],
-		diplomas: [],
-		certifications: [],
-		driving_licenses: ["C"],
-	},
-
-	convoyeur_fonds: {
-		cnaps: ["transport_fonds"],
+		cnaps: ["TRANSPORT_FONDS"],
 		diplomas: [],
 		certifications: [],
 		driving_licenses: ["C"],
