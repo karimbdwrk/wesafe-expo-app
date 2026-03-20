96712bb0e88c33b549af597912994753dcb74639
Karim Badwork <karim@badwork.fr>
2026-03-21 00:43:50 +0100

    feat(suggestedjobs): ajouter le composant SuggestedJobs pour afficher les emplois suggérés

---
+++ components/SuggestedJobs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { VStack } from "@/components/ui/vstack";
+import { Heading } from "@/components/ui/heading";
+
+const SuggestedJobs = () => {
+	return (
+		<VStack space={4} mt={4}>
+			<Heading>Suggested Jobs</Heading>
+		</VStack>
+	);
+};
+
+export default SuggestedJobs;
