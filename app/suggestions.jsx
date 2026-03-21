cdbe11379e5ec81bf282f434c84e343bad13d89d
Karim Badwork <karim@badwork.fr>
2026-03-21 01:43:33 +0100

    feat(suggestions): ajouter le composant SuggestionsScreen pour afficher les suggestions

---
+++ app/suggestions.jsx
@@ -0,0 +1,14 @@
+import { VStack } from "@/components/ui/vstack";
+import { Heading } from "@/components/ui/heading";
+
+const SuggestionsScreen = () => {
+	return (
+		<VStack className='flex-1 p-4'>
+			<Heading size='lg' className='mb-4'>
+				Suggestions screen
+			</Heading>
+		</VStack>
+	);
+};
+
+export default SuggestionsScreen;
