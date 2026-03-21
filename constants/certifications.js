64d199837822d9a7b17e678635c702075c160e71
Karim Badwork <karim@badwork.fr>
2026-03-21 03:53:58 +0100

    feat(certifications): ajouter la propriété validity_years pour chaque certification

--- constants/certifications.js
+++ constants/certifications.js
@@ -3,48 +3,56 @@ export const CERTIFICATIONS = {
 		acronym: "SST",
 		name: "Sauveteur Secouriste du Travail",
 		category: "secourisme",
+		validity_years: 2,
 	},
 
 	ho_bo: {
 		acronym: "H0B0",
 		name: "Habilitation électrique non électricien",
 		category: "habilitation",
+		validity_years: 3,
 	},
 
 	palpation: {
 		acronym: "PALP",
 		name: "Formation palpation de sécurité",
 		category: "evenementiel",
+		validity_years: null,
 	},
 
 	formation_tir: {
 		acronym: "TIR",
 		name: "Formation au tir",
 		category: "protection_rapprochee",
+		validity_years: null,
 	},
 
 	secourisme_tactique: {
 		acronym: "TACTICAL MED",
 		name: "Secourisme tactique",
 		category: "protection_rapprochee",
+		validity_years: null,
 	},
 
 	formation_aeroportuaire: {
 		acronym: "AERO",
 		name: "Formation sûreté aéroportuaire",
 		category: "surete_aeroportuaire",
+		validity_years: null,
 	},
 
 	recyclage_ssiap: {
 		acronym: "RECY SSIAP",
 		name: "Recyclage SSIAP",
 		category: "securite_incendie",
+		validity_years: 3,
 	},
 
 	habilitation_chien: {
 		acronym: "CHIEN",
 		name: "Habilitation conducteur cynophile",
 		category: "cynophile",
+		validity_years: null,
 	},
 };
 
