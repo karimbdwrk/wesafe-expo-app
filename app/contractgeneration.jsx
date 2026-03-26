36ab5a6117751d61ad4f269699e8a00f4da6d9d4
Karim Badwork <karim@badwork.fr>
2026-03-26 20:44:54 +0100

    feat(ContractGeneration): ajouter l'écran de génération de contrat

---
+++ app/contractgeneration.jsx
@@ -0,0 +1,12 @@
+import { VStack } from "@components/ui/vstack";
+import { Heading } from "@components/ui/heading";
+
+const ContractGenerationScreen = () => {
+	return (
+		<VStack>
+			<Heading>Contract Generation</Heading>
+		</VStack>
+	);
+};
+
+export default ContractGenerationScreen;
