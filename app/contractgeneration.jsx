60ab6ff309a4a146bcf908aa42804690172f95e2
Karim Badwork <karim@badwork.fr>
2026-03-27 18:37:07 +0100

    feat(ContractGeneration): afficher l'ID de la candidature dans l'écran de génération de contrat

--- app/contractgeneration.jsx
+++ app/contractgeneration.jsx
@@ -1,10 +1,17 @@
+import React from "react";
+
+import { useLocalSearchParams } from "expo-router";
+
 import { VStack } from "@/components/ui/vstack";
 import { Heading } from "@/components/ui/heading";
+import { Text } from "@/components/ui/text";
 
 const ContractGenerationScreen = () => {
+	const { application_id } = useLocalSearchParams();
 	return (
 		<VStack>
 			<Heading>Contract Generation</Heading>
+			<Text>Application ID: {application_id}</Text>
 		</VStack>
 	);
 };
