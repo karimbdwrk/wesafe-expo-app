36a36a13e9be80bfe761f07432bae68d29a439d4
Karim Badwork <karim@badwork.fr>
2026-04-01 14:53:46 +0200

    refactor(Analytics): améliorer la gestion des thèmes et ajuster le style du ScrollView

--- app/analytics.jsx
+++ app/analytics.jsx
@@ -1,9 +1,22 @@
 import HomeChartsPro from "../components/HomeChartsPro";
 import { ScrollView } from "react-native-gesture-handler";
+import { useTheme } from "@/context/ThemeContext";
+import Colors from "@/constants/Colors";
 
 const Analytics = () => {
+	const { isDark } = useTheme();
+
 	return (
-		<ScrollView style={{ flex: 1, padding: 16 }}>
+		<ScrollView
+			style={{
+				flex: 1,
+				padding: 10,
+				paddingTop: 15,
+				paddingBottom: 30,
+				backgroundColor: isDark
+					? Colors.dark.background
+					: Colors.light.background,
+			}}>
 			<HomeChartsPro />
 		</ScrollView>
 	);
