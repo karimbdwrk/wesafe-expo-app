771e2baef5db3b6f1add73affa81b88ee57bc45f
Karim Badwork <karim@badwork.fr>
2026-04-03 18:13:00 +0200

    feat(languages): ajouter un fichier de constantes pour les langues disponibles

---
+++ constants/languages.js
@@ -0,0 +1,46 @@
+export const languages = [
+	{
+		code: "FR",
+		name: "Français",
+	},
+	{
+		code: "EN",
+		name: "Anglais",
+	},
+	{
+		code: "AR",
+		name: "Arabe",
+	},
+	{
+		code: "ES",
+		name: "Espagnol",
+	},
+	{
+		code: "PT",
+		name: "Portugais",
+	},
+	{
+		code: "IT",
+		name: "Italien",
+	},
+	{
+		code: "TR",
+		name: "Turc",
+	},
+	{
+		code: "ZH",
+		name: "Mandarin",
+	},
+	{
+		code: "JA",
+		name: "Japonais",
+	},
+	{
+		code: "RU",
+		name: "Russe",
+	},
+	{
+		code: "DE",
+		name: "Allemand",
+	},
+];
