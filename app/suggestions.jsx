0068fded522b0e07a8164e2f9d54db0c3e62494c
Karim Badwork <karim@badwork.fr>
2026-04-05 20:59:14 +0200

    feat(SuggestionsScreen): ajouter l'affichage du code de région et améliorer la mise en page

--- app/suggestions.jsx
+++ app/suggestions.jsx
@@ -1,12 +1,25 @@
+import { useLocalSearchParams } from "expo-router";
+
 import { VStack } from "@/components/ui/vstack";
 import { Heading } from "@/components/ui/heading";
+import { Text } from "@/components/ui/text";
+import { useTheme } from "@/context/ThemeContext";
+import Colors from "@/constants/Colors";
 
 const SuggestionsScreen = () => {
+	const params = useLocalSearchParams();
+	const regionCode = params.region_code;
+	const { isDark } = useTheme();
+	const textPrimary = isDark ? Colors.dark.text : Colors.light.text;
+
 	return (
 		<VStack className='flex-1 p-4'>
-			<Heading size='lg' className='mb-4'>
+			<Heading size='lg' className='mb-4' style={{ color: textPrimary }}>
 				Suggestions screen
 			</Heading>
+			<Text style={{ color: textPrimary }}>
+				Region code: {regionCode}
+			</Text>
 		</VStack>
 	);
 };
