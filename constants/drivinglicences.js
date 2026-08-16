ad6c6ff5f196a99694293fa90f360187973b0368
Karim Badwork <karim@badwork.fr>
2026-08-16 03:47:32 +0200

    refactor(drivinglicences): simplifier la liste des permis de conduire en supprimant les types redondants

--- constants/drivinglicences.js
+++ constants/drivinglicences.js
@@ -1,19 +1,7 @@
 export const DRIVING_LICENSES = {
 	a: {
 		acronym: "A",
-		name: "Permis moto toutes cylindrées",
-		category: "moto",
-	},
-
-	a1: {
-		acronym: "A1",
-		name: "Permis moto légère",
-		category: "moto",
-	},
-
-	a2: {
-		acronym: "A2",
-		name: "Permis moto intermédiaire",
+		name: "Permis moto",
 		category: "moto",
 	},
 
@@ -23,63 +11,15 @@ export const DRIVING_LICENSES = {
 		category: "vehicule_leger",
 	},
 
-	be: {
-		acronym: "BE",
-		name: "Permis voiture + remorque",
-		category: "vehicule_leger",
-	},
-
-	b96: {
-		acronym: "B96",
-		name: "Extension remorque permis B",
-		category: "vehicule_leger",
-	},
-
 	c: {
 		acronym: "C",
 		name: "Permis poids lourd",
 		category: "poids_lourd",
 	},
 
-	c1: {
-		acronym: "C1",
-		name: "Permis poids lourd intermédiaire",
-		category: "poids_lourd",
-	},
-
-	ce: {
-		acronym: "CE",
-		name: "Permis poids lourd + remorque",
-		category: "poids_lourd",
-	},
-
-	c1e: {
-		acronym: "C1E",
-		name: "Permis poids lourd intermédiaire + remorque",
-		category: "poids_lourd",
-	},
-
 	d: {
 		acronym: "D",
 		name: "Permis transport de personnes",
-		category: "transport_personnes",
-	},
-
-	d1: {
-		acronym: "D1",
-		name: "Permis transport personnes intermédiaire",
-		category: "transport_personnes",
-	},
-
-	de: {
-		acronym: "DE",
-		name: "Permis transport personnes + remorque",
-		category: "transport_personnes",
-	},
-
-	d1e: {
-		acronym: "D1E",
-		name: "Permis transport personnes intermédiaire + remorque",
 		category: "transport_personnes",
 	},
 };
